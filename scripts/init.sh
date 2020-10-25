fb4ed370dcb6033dadf49a49b5ca063a942a33fa
Albert <oldcai@live.com>
2020-10-26 00:57:03 +0800

    use pypi mirror to accelerate image building process

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pwd
-#export MIRROR="-i https://pypi.doubanio.com/simple"
-export MIRROR="-i https://mirrors.cloud.tencent.com/pypi/simple"
+export MIRROR="-i https://pypi.doubanio.com/simple"
+#export MIRROR="-i https://mirrors.cloud.tencent.com/pypi/simple"
 pip install $MIRROR -U pip
 export LIBRARY_PATH=/lib:/usr/lib
 pip install $MIRROR --no-cache-dir -r requirements.txt
