95f013c88ee5ee1a37118f904ea90cab614b9ed4
Albert <oldcai@live.com>
2020-10-25 21:09:27 +0800

    import static files

---
+++ loyalty_program/static/js/main.js
@@ -0,0 +1,127 @@
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Focus input ]*/
+    $('.input100').each(function () {
+        $(this).on('blur', function () {
+            if ($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            } else {
+                $(this).removeClass('has-val');
+            }
+        })
+    })
+
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit', function () {
+        var check = true;
+
+        for (var i = 0; i < input.length; i++) {
+            if (validate(input[i]) == false) {
+                showValidate(input[i]);
+                check = false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function () {
+        $(this).focus(function () {
+            hideValidate(this);
+        });
+    });
+
+    function validate(input) {
+        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        } else {
+            if ($(input).val().trim() == '') {
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+
+    /*==================================================================
+    [ Show pass ]*/
+    var showPass = 0;
+    $('.btn-show-pass').on('click', function () {
+        if (showPass == 0) {
+            $(this).next('input').attr('type', 'text');
+            $(this).addClass('active');
+            showPass = 1;
+        } else {
+            $(this).next('input').attr('type', 'password');
+            $(this).removeClass('active');
+            showPass = 0;
+        }
+
+    });
+
+    /*==================================================================
+    [Navigation bar]*/
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }
+
+    /*==================================================================
+    [gallery]*/
+
+    //scroll slides on swipe for touch enabled devices
+
+    $(".carousel").on("touchstart", function (event) {
+
+        var yClick = event.originalEvent.touches[0].pageY;
+        $(this).one("touchmove", function (event) {
+
+            var yMove = event.originalEvent.touches[0].pageY;
+            if (Math.floor(yClick - yMove) > 1) {
+                $(".carousel").carousel('next');
+            } else if (Math.floor(yClick - yMove) < -1) {
+                $(".carousel").carousel('prev');
+            }
+        });
+        $(".carousel").on("touchend", function () {
+            $(this).off("touchmove");
+        });
+    });
+    $(window).on('load', function () {
+        var i;
+        for (i = 0; i < $('#myUserCarousel').find('.carousel-inner').children().length; i++) {
+            $("#myUserCarousel ol").append('<li data-target="#myUserCarousel" data-slide-to=' + i + '></li>');
+        }
+        for (i = 0; i < $('#myGeneralCarousel').find('.carousel-inner').children().length; i++) {
+            $("#myGeneralCarousel ol").append('<li data-target="#myGeneralCarousel" data-slide-to=' + i + '></li>');
+        }
+        $('#myUserCarousel li:first').addClass('active');
+        $('#myGeneralCarousel li:first').addClass('active');
+    });
+
+
+})(jQuery);
