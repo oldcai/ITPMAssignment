5362ad7ceb3dbd5fc880fe0df6d6f9f558581132
Albert <oldcai@live.com>
2020-10-27 02:29:27 +0800

    ensure db dir exists

--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 GUNICORN_LOG_LEVEL=${LOG_LEVEL:-DEBUG}
 python manage.py collectstatic --noinput &
+mkdir -p /code/db
 python manage.py migrate --noinput
 gunicorn -c gunicorn.py itpm.wsgi --log-level=$GUNICORN_LOG_LEVEL
