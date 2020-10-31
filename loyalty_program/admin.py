9ca77dfb635c4ac5ea35497fdc75b18ab463cafd
Albert <oldcai@live.com>
2020-11-01 00:04:23 +0800

    Offer model admin view with rich text editor

--- loyalty_program/admin.py
+++ loyalty_program/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from quilljs.admin import QuillAdmin
 
-from loyalty_program.models import Credit
+from loyalty_program.models import Credit, Offer, UserOfferApplication
 
 
 @admin.register(Credit)
@@ -8,5 +9,31 @@ class CreditAdmin(admin.ModelAdmin):
     list_display = ['user', 'points']
     search_fields = ['user__username']
 
-    list_filter = ['points']
     raw_id_fields = ['user']
+
+
+@admin.register(Offer)
+class OfferAdmin(QuillAdmin):
+    list_display = ['title', 'price_in_points']
+
+    list_filter = ['enabled']
+
+    actions = ['enable', 'disable']
+
+    def enable(self, request, queryset):
+        queryset.update(enabled=True)
+
+    def disable(self, request, queryset):
+        queryset.update(enabled=False)
+
+
+@admin.register(UserOfferApplication)
+class UserOfferApplication(admin.ModelAdmin):
+    list_display = ['user', 'offer', 'permitted']
+
+    list_filter = ['permitted']
+
+    actions = ['permit']
+
+    def permit(self, request, queryset):
+        queryset.update(permitted=True)
--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -2,5 +2,6 @@
 GUNICORN_LOG_LEVEL=${LOG_LEVEL:-DEBUG}
 python manage.py collectstatic --noinput &
 mkdir -p /code/db
+mkdir -p /mnt/static
 python manage.py migrate --noinput
 gunicorn -c gunicorn.py itpm.wsgi --log-level=$GUNICORN_LOG_LEVEL
