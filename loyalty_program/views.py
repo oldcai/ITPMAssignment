6310d958221264a7fdfee83e00708590ef49028f
Albert <oldcai@live.com>
2020-11-01 01:55:06 +0800

    add footer

--- loyalty_program/views.py
+++ loyalty_program/views.py
@@ -14,7 +14,7 @@ class HomepageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomepageView, self).get_context_data(**kwargs)
         context.update({
-            'offers': Offer.objects.all()[:5],
+            'offers': Offer.objects.filter(enabled=True)[:5],
         })
         return context
 
