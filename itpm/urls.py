9945af4902411c2aec834727aa6754ee4cbeb8da
Albert <oldcai@live.com>
2020-11-02 18:51:49 +0800

    about us view

--- itpm/urls.py
+++ itpm/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 from loyalty_program.views import ApplyOfferView, HomepageView, OfferDetailView, ProfileView
 
@@ -29,6 +30,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path(f'offer/<pk>', login_required(OfferDetailView.as_view()), name='offer-detail-view'),
     path(f'apply-offer/', login_required(ApplyOfferView.as_view()), name='apply-offer-view'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
 ]
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
